91b6a44bfbcbf69d24afc9f02488cfffe8b3e9b6
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2017-10-10 22:47:53 -0700

    updated index, authentication, and database schema

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-
 $(function() {
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDmaOeZHpED-rovVs3wOEJYcktlN40us-M",
-    authDomain: "fir-8037d.firebaseapp.com",
-    databaseURL: "https://fir-8037d.firebaseio.com",
-    projectId: "fir-8037d",
-    storageBucket: "fir-8037d.appspot.com",
-    messagingSenderId: "677029590717"
-  };
-  firebase.initializeApp(config);
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDmaOeZHpED-rovVs3wOEJYcktlN40us-M",
+        authDomain: "fir-8037d.firebaseapp.com",
+        databaseURL: "https://fir-8037d.firebaseio.com",
+        projectId: "fir-8037d",
+        storageBucket: "fir-8037d.appspot.com",
+        messagingSenderId: "677029590717"
+    };
+    firebase.initializeApp(config);
 
     "use strict";
     $('#login-form-link').click(function(e) {
@@ -29,50 +28,93 @@ $(function() {
 
     // Firebase Authentication
 
+    //Login Elements
     const loginUsername = document.getElementById('loginUsername');
     const loginPassword = document.getElementById('loginPassword');
-    const signupEmail = document.getElementById('signupEmail');
-    const signupUsername = document.getElementById('signupUsername');
-    const signupPassword = document.getElementById('signupPassword');
     const btnLogin = document.getElementById('login-submit');
-    const btnSignUp = document.getElementById('register-submit');
     const btnLogOut = document.getElementById('logout-submit');
 
+    //Sign Up Elements
+    const signUpUsername = document.getElementById('signUpUsername');
+    const signUpEmail = document.getElementById('signUpEmail');
+    const signUpPassword = document.getElementById('signUpPassword');
+    const confirmPassword = document.getElementById('confirmPassword');
+    const btnsignUp = document.getElementById('register-submit');
+
+    //Login Event
     btnLogin.addEventListener('click', e => {
-      //Get username and pass
-      const username = loginUsername.value;
-      const pass = txtPassword.value;
-      const auth = firebase.auth();
+        //Get username and pass
+        const username = loginUsername.value;
+        const pass = loginPassword.value;
+        const auth = firebase.auth();
 
-      //Sign In
-      const promise = auth.signInWithusernameAndPassword(username, pass);
-      promise.catch(e => console.log(e.message));
+        //Sign In
+        const promise = auth.signInWithEmailAndPassword(username, pass);
+        promise.catch(e => console.log(e.message));
 
     });
 
-    btnSignUp.addEventListener('click', e => {
-      //Get email and pass
-      //TODO: Check for real email
-      const email = signupEmail.value;
-      const pass = signupPassword.value;
-      const auth = firebase.auth();
+    //Sign Up Event
+    btnsignUp.addEventListener('click', e => {
+        /*
+        Get email and pass
+        TODO: Email verification
+        */
+        const email = signUpEmail.value;
+        const pass = signUpPassword.value;
+        const auth = firebase.auth();
 
-      //Sign in
-      const promise = auth.createUserWithEmailAndPassword(email , pass);
-       promise.catch(e => console.log(e.message));
+        //Sign in
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
     });
 
+    //Log Out Event
     btnLogOut.addEventListener('click', e => {
-      firebase.auth().signOut();
+        firebase.auth().signOut();
     });
 
+    //Log in log
     firebase.auth().onAuthStateChanged(firebaseUser => {
-      if(firebaseUser) {
-        console.log(firebaseUser);
-          }
-      else {
-        console.log('not logged in');
-      }
+        if (firebaseUser) {
+            console.log(firebaseUser);
+        } else {
+            console.log('not logged in');
+        }
+    });
+    //#Content Display
+
+    //Get Eelements
+    const preObject = document.getElementById('users');
+    const ulList = document.getElementById('list');
+
+    //Create references
+    const dbRefObject = firebase.database().ref().child('users');
+    const dbRefList = dbRefObject.child('user1')
+
+    //Sync objet changes
+    dbRefObject.on('value', snap => {
+        preObject.innertext = JSON.stringify(snap.val(), null, 3);
+    })
+
+    //Sync list changes
+    dbRefList.on('child_added', snap => {
+      const li = document.createElement('li');
+      li.id = snap.key;
+      li.innerText = snap.val();
+      ulList.appendChild(li);
+    });
+
+    //Sync database value changes
+    dbRefList.on('child_changed', snap => {
+      const liChanged = document.getElementById(snap.key);
+      liChanged.innerText = snap.val();
+    });
+
+    //Sync database value removals
+    dbRefList.on('child_removed', snap => {
+      const liToRemove = document.getElementById(snap.key);
+      liToRemove.remove();
     });
 
 });
